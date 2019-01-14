fb088bbaa021924716a7f91ba1ea359e9f795a30
lisu9090 <lisu9090@gmail.com>
2019-01-14 20:54:47 +0100

    Poruszanie robotem ogarniete, trzeba pobawic sie w ustawianie wlasciwosci obiektow. Zrobic sterowanie przy pomocy urzadzenia haptycznego

--- haptic-game/Assets/Haptic Project Components/Robot_arm_level/Scripts/RobotMovement.cs
+++ haptic-game/Assets/Haptic Project Components/Robot_arm_level/Scripts/RobotMovement.cs
@@ -15,67 +15,42 @@ public class RobotMovement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetKey(KeyCode.UpArrow) || movementDirection > 0)
+        if (Input.GetKey(KeyCode.UpArrow) && !Input.GetKey(KeyCode.DownArrow))
         {
-
-
-            //    if (speedUp < movementSpeed && Input.GetKey(KeyCode.UpArrow))
-            //    {
-            //        if (movementDirection < 0)
-            //        {
-            //            speedUp -= (movementSpeed / 15);
-            //        }
-            //        else
-            //        {
-            //            speedUp += (movementSpeed / 30);
-            //            movementDirection = 1;
-            //        }
-            //    }
-            //    else
-            //    {
-            //        if (speedUp > 0)
-            //            speedUp -= (movementSpeed / 30);
-            //    }
-
-            Vector3 position = cam.transform.position;
-            position.y = 0;
-            position.x = (this.transform.position.x - position.x);
-            position.z = (this.transform.position.z - position.z);
-            position = position.normalized;
-            position.Scale(new Vector3(speedUp, 0, speedUp));
-            this.transform.position += position;
+            if (movementDirection < 0)
+                speedUp -= (movementSpeed/15);
+            else
+            {
+                movementDirection = 1;
+                if(speedUp < movementSpeed)
+                    speedUp += (movementSpeed / 30);
+            }
+            moveRobot();
         }
-        if (Input.GetKey(KeyCode.DownArrow) || movementDirection < 0)
+        if(Input.GetKey(KeyCode.DownArrow) && !Input.GetKey(KeyCode.UpArrow))
         {
-            //if (speedUp < movementSpeed && Input.GetKey(KeyCode.DownArrow))
-            //{
-            //    if (movementDirection > 0)
-            //    {
-            //        speedUp -= (movementSpeed / 15);
-            //    }
-            //    else
-            //    {
-            //        speedUp += (movementSpeed / 30);
-            //        movementDirection = -1;
-            //    }
-            //}
-            //else
-            //{
-            //    if (speedUp > 0)
-            //        speedUp -= (movementSpeed / 30);
-            //}
-
-            Vector3 position = cam.transform.position;
-            position.y = 0;
-            position.x -= this.transform.position.x;
-            position.z -= this.transform.position.z;
-            position = position.normalized;
-            position.Scale(new Vector3(speedUp, 0, speedUp));
-            this.transform.position += position;
+            if (movementDirection > 0)
+                speedUp -= (movementSpeed / 15);
+            else
+            {
+                movementDirection = -1;
+                if (speedUp < movementSpeed)
+                    speedUp += (movementSpeed / 30);
+            }
+            moveRobot();
         }
+        if ((!(Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.DownArrow)) || (Input.GetKey(KeyCode.UpArrow) && Input.GetKey(KeyCode.DownArrow))) && speedUp > 0)
+        {
+            speedUp -= (movementSpeed / 30);
+            moveRobot();
+        }
+
         if (speedUp <= 0)
         {
-            movementDirection = 0;
+            if(movementDirection != 0)
+                movementDirection = 0;
+            if(speedUp < 0)
+                speedUp = 0;
         }
 
         if (Input.GetKey(KeyCode.LeftArrow))
@@ -86,5 +61,24 @@ public class RobotMovement : MonoBehaviour {
         {
             this.transform.Rotate(new Vector3(0, rotationSpeed, 0));
         }
+    }
+
+    private void moveRobot()
+    {
+        Vector3 position = cam.transform.position;
+        position.y = 0;
+        if (movementDirection > 0)
+        {
+            position.x = (this.transform.position.x - position.x);
+            position.z = (this.transform.position.z - position.z);
+        }
+        else if (movementDirection < 0)
+        {
+            position.x -= this.transform.position.x;
+            position.z -= this.transform.position.z;
+        }
+        position = position.normalized;
+        position.Scale(new Vector3(speedUp, 0, speedUp));
+        this.transform.position += position;
     }
 }
